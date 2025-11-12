763dfab9bbd80cb5129884a26d043e6a3decf2ba
gar <m.biedermann@campus.tu-berlin.de>
2025-11-12 22:00:14 +0100

    add: restart wifi for iwctl

--- extensions/wifi-commander/src/restart-wifi.tsx
+++ extensions/wifi-commander/src/restart-wifi.tsx
@@ -1,29 +1,66 @@
 import { getPreferenceValues, showToast } from "@vicinae/api";
 import { executeNmcliCommand, executeIwctlCommand, type ExecResult } from "./utils/execute";
+import { getIwctlAdapter } from "./utils/wifi-helpers";
 
 export default async function RestartWifi() {
   const networkCliTool = getPreferenceValues<{ "network-cli-tool": string }>();
+  let onResult: ExecResult;
+
   await showToast({
     title: "Restarting Wi-Fi",
     message: "Please wait while Wi-Fi services restart...",
   });
 
-  // Turn off Wi-Fi
-  const offResult = await executeNmcliCommand("radio wifi off");
 
-  if (!offResult.success) {
-    await showToast({
-      title: "Failed to Restart Wi-Fi",
-      message: offResult.error || "Could not turn off Wi-Fi",
-    });
-    return;
+
+  switch (networkCliTool["network-cli-tool"]) {
+    case "nmcli":
+      // Turn off Wi-Fi
+      const offResult = await executeNmcliCommand("radio wifi off");
+
+      if (!offResult.success) {
+        await showToast({
+          title: "Failed to Restart Wi-Fi",
+          message: offResult.error || "Could not turn off Wi-Fi",
+        });
+        return;
+      }
+
+      // Small delay to ensure clean shutdown
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // Turn on Wi-Fi
+      onResult = await executeNmcliCommand("radio wifi on");
+      break;
+
+    case "iwctl": {
+      const adapterName = await getIwctlAdapter()
+      if (!adapterName.success){
+        onResult = adapterName;
+        break;
+      }
+
+      // Turn off Wi-Fi
+      const offResult = await executeIwctlCommand("adapter", [adapterName["stdout"], "set-property", "Powered", "off"]);
+
+      if (!offResult.success) {
+        await showToast({
+          title: "Failed to Restart Wi-Fi",
+          message: offResult.error || "Could not turn off Wi-Fi",
+        });
+        return;
+      }
+
+      // Small delay to ensure clean shutdown
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Turn on Wi-Fi
+      onResult = await executeIwctlCommand("adapter", [adapterName["stdout"], "set-property", "Powered", "on"]);
+      break;
+    }
+    default:
+      throw new Error("Invalid network CLI tool: " + networkCliTool["network-cli-tool"]);
   }
-
-  // Small delay to ensure clean shutdown
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  // Turn on Wi-Fi
-  const onResult = await executeNmcliCommand("radio wifi on");
 
   if (onResult.success) {
     await showToast({
