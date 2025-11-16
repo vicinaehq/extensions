ff75fa3182036e8800622382e5f08a2984bb4732
Aurelien Brabant <gh@aurelle.dev>
2025-11-16 21:30:46 +0100

    feat: handle light/dark variant properly

--- extensions/dashboard-icons/src/search-icons.tsx
+++ extensions/dashboard-icons/src/search-icons.tsx
@@ -41,8 +41,8 @@ const CopyFormatAction = ({
 	format: Format;
 	icon: DashboardIcon;
 }) => {
-	const download = async () => {
-		const url = formatUrl(icon.name, format);
+	const download = async (name: string) => {
+		const url = formatUrl(name, format);
 		const toast = await showToast(Toast.Style.Animated, "Download icon...");
 		const res = await fetch(url);
 
@@ -51,7 +51,7 @@ const CopyFormatAction = ({
 		} else {
 			const buf = await res.bytes();
 			const iconDir = path.join(environment.supportPath, "icons");
-			const target = path.join(iconDir, `${icon.name}.${format}`);
+			const target = path.join(iconDir, `${name}.${format}`);
 			await fsp.mkdir(iconDir, { recursive: true });
 			await fsp.writeFile(target, buf);
 			await Clipboard.copy({ file: target });
@@ -62,12 +62,72 @@ const CopyFormatAction = ({
 		await showHUD("Copied to clipboard");
 	};
 
+	const makeDownload = (name: string) => () => download(name);
+
+	if (icon.colors) {
+		return (
+			<>
+				{icon.colors.dark && (
+					<Action
+						title={`Copy dark variant (${format})`}
+						icon={Icon.Moon}
+						onAction={makeDownload(icon.colors.dark)}
+					/>
+				)}
+				{icon.colors.light && (
+					<Action
+						title={`Copy light variant (${format})`}
+						icon={Icon.Sun}
+						onAction={makeDownload(icon.colors.light)}
+					/>
+				)}
+			</>
+		);
+	}
+
 	return (
 		<Action
 			title={`Copy as ${format}`}
 			icon={Icon.CopyClipboard}
-			onAction={download}
+			onAction={makeDownload(icon.name)}
 		></Action>
+	);
+};
+
+const CopyFormatUrlAction = ({
+	icon,
+	format,
+}: {
+	format: Format;
+	icon: DashboardIcon;
+}) => {
+	if (icon.colors) {
+		return (
+			<>
+				{icon.colors.dark && (
+					<Action.CopyToClipboard
+						title={`Copy dark URL (${format})`}
+						icon={Icon.Moon}
+						content={formatUrl(icon.colors.dark, format)}
+					/>
+				)}
+				{icon.colors.light && (
+					<Action.CopyToClipboard
+						title={`Copy light URL (${format})`}
+						icon={Icon.Sun}
+						content={formatUrl(icon.colors.light, format)}
+					/>
+				)}
+			</>
+		);
+	}
+
+	return (
+		<Action.CopyToClipboard
+			title={`Copy URL (${format})`}
+			icon={Icon.CopyClipboard}
+			content={formatUrl(icon.name, format)}
+		/>
 	);
 };
 
@@ -82,19 +142,23 @@ const IconCell = ({ icon }: { icon: DashboardIcon }) => {
 			keywords={[...icon.categories, ...icon.aliases]}
 			actions={
 				<ActionPanel>
-					<CopyFormatAction icon={icon} format={copyFormat} />
-					{formats
-						.filter((fmt) => fmt !== copyFormat)
-						.map((fmt) => (
-							<CopyFormatAction key={`copy-${fmt}`} icon={icon} format={fmt} />
+					<ActionPanel.Section>
+						<CopyFormatAction icon={icon} format={copyFormat} />
+						{formats
+							.filter((fmt) => fmt !== copyFormat)
+							.map((fmt) => (
+								<CopyFormatAction
+									key={`copy-${fmt}`}
+									icon={icon}
+									format={fmt}
+								/>
+							))}
+					</ActionPanel.Section>
+					<ActionPanel.Section>
+						{formats.map((fmt) => (
+							<CopyFormatUrlAction format={fmt} icon={icon} />
 						))}
-					{formats.map((fmt) => (
-						<Action.CopyToClipboard
-							key={`copy-url-${fmt}`}
-							title={`Copy ${fmt} URL`}
-							content={formatUrl(icon.name, fmt)}
-						/>
-					))}
+					</ActionPanel.Section>
 					<Action.CopyToClipboard
 						title={`Copy icon name`}
 						content={icon.name}
