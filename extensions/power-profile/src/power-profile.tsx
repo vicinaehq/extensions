6b44bdea9778a148878bca68873c132181d8c16c
Alex Ozerov <botkooper@gmail.com>
2025-11-20 08:46:33 +0200

    add link to nix pkg

--- extensions/power-profile/src/power-profile.tsx
+++ extensions/power-profile/src/power-profile.tsx
@@ -15,6 +15,7 @@ export default function Extension() {
 
 - Upstream URL (with docs): [https://gitlab.freedesktop.org/upower/power-profiles-daemon](https://gitlab.freedesktop.org/upower/power-profiles-daemon)
 - Arch package: [https://archlinux.org/packages/extra/x86_64/power-profiles-daemon/](https://archlinux.org/packages/extra/x86_64/power-profiles-daemon/)
+- Nix package: [https://search.nixos.org/packages?channel=25.05&show=power-profiles-daemon&query=power-profiles-daemon](https://search.nixos.org/packages?channel=25.05&show=power-profiles-daemon&query=power-profiles-daemon)
 - Debian package: [https://packages.debian.org/sid/power-profiles-daemon](https://packages.debian.org/sid/power-profiles-daemon)
 `}
 				/>
