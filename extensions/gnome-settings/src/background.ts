cb9666e75869e36512168cbdffbea51b051e85b4
Shahil Jain <75173792+shahiljain@users.noreply.github.com>
2026-04-01 15:20:25 +0530

    Updating the Background Setting String in Gnome-Settings

--- extensions/gnome-settings/src/background.ts
+++
@@ -1,7 +0,0 @@
-import { closeMainWindow } from "@vicinae/api";
-import { spawn } from "node:child_process";
-
-export default () => {
-    closeMainWindow();
-    spawn(`gnome-control-center`, ["background"]);
-}
